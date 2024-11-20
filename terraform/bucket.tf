61a8351daa472ede4312a0beac24741fcb294b46
VJACHESLAV <114395964+slava1005@users.noreply.github.com>
2024-11-20 15:35:09 +0300

    Create bucket.tf

---
+++ terraform/bucket.tf
@@ -0,0 +1,33 @@
+locals {
+    current_timestamp = timestamp()
+    formatted_date = formatdate("DD-MM-YYYY", local.current_timestamp)
+    bucket_name = "savilovvv-${local.formatted_date}"
+}
+
+// Создаем сервисный аккаунт для backet
+resource "yandex_iam_service_account" "service" {
+  folder_id = var.folder_id
+  name      = "bucket-sa"
+}
+
+// Назначение роли сервисному аккаунту
+resource "yandex_resourcemanager_folder_iam_member" "bucket-editor" {
+  folder_id = var.folder_id
+  role      = "storage.editor"
+  member    = "serviceAccount:${yandex_iam_service_account.service.id}"
+  depends_on = [yandex_iam_service_account.service]
+}
+
+// Создание статического ключа доступа
+resource "yandex_iam_service_account_static_access_key" "sa-static-key" {
+  service_account_id = yandex_iam_service_account.service.id
+  description        = "static access key for object storage"
+}
+
+// Создание бакета с использованием ключа
+resource "yandex_storage_bucket" "fedorchukds" {
+  access_key = yandex_iam_service_account_static_access_key.sa-static-key.access_key
+  secret_key = yandex_iam_service_account_static_access_key.sa-static-key.secret_key
+  bucket = local.bucket_name
+  acl    = "public-read"
+}

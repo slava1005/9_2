e7340676300415c99204a1d7b37b5b2dca9e8f57
VJACHESLAV <114395964+slava1005@users.noreply.github.com>
2024-11-23 13:36:16 +0300

    Update upload_image.tf

--- terraform/upload_image.tf
+++ terraform/upload_image.tf
@@ -2,8 +2,8 @@ resource "yandex_storage_object" "deadline-picture" {
   access_key = yandex_iam_service_account_static_access_key.sa-static-key.access_key
   secret_key = yandex_iam_service_account_static_access_key.sa-static-key.secret_key
   bucket = local.bucket_name
-  key    = "deadline-cat.jpg"
-  source = "~/deadline-cat.jpg"
+  key    = "flagimperii.jpg"
+  source = "~/flagimperii.jpg"
   acl = "public-read"
   depends_on = [yandex_storage_bucket.savilovvv]
 }
